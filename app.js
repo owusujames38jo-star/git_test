848676fd7aef32c1b80bae7b38b10d2e7910ea24
jamesiva1 <236145571+owusujames38jo-star@users.noreply.github.com>
2025-11-08 16:58:33 -0500

    modified header and text to body

---
+++ app.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', () => {
+// const itemsContainer = document.querySelector('.back-images-container');
+// const slides = document.querySelectorAll('.back');
+// const totalSlides = slides.length;
+// let currentSlide = 0;
+// const duration = 3000;
+
+// function nextSlide() {
+//     currentSlide = (currentSlide + 1) % totalSlides;
+//     const offset = -currentSlide * 100;
+//     itemsContainer.style.transform = `translateX(${offset}%)`;
+// }
+
+// setInterval(nextSlide, duration);
+// })
